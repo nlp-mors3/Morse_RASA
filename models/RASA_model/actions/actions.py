717386c177c0ab76e2d038b1d4e794f4a8b03a36
Yuen Ka Hang Christian <2214959@slu.edu.ph>
2025-12-04 21:02:43 +0800

    adding basic foundation for the impl of rasa

--- models/RASA_model/actions/actions.py
+++ models/RASA_model/actions/actions.py
@@ -3,6 +3,9 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+#import for the json request
+import requests
+
 # Load the CSV once when the server starts
 # Ensure your CSV headers match the code below
 df = pd.read_csv('ibaloi_lexicon.csv')
@@ -46,3 +49,40 @@ class ActionTranslateToIbaloi(Action):
             dispatcher.utter_message(text=f"Sorry, I don't have a translation for '{query_word}' in my database yet.")
 
         return []
+    
+    # --- Custom RAG Fallback Action ---
+class ActionRAGCall(Action):
+    def name(self) -> str:
+        return "action_rag_call"
+
+    async def run(self,
+                  dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        #fix this code to get user message from 
+        user_message = tracker.latest_message.get("text", "").strip().lower()
+        try:
+
+            rag_url = "http://rag_server:8000/predict"
+            response = requests.post(
+                rag_url,
+                json={"query": user_message, 
+                }
+            )
+
+            # Get the full answer from the response as a JSON object
+            if response.status_code == 200:  # Check if the request was successful
+                response_json = response.json()
+                full_answer = response_json.get("text", "") 
+
+                # Send the full answer as a message
+                dispatcher.utter_message(text=full_answer)
+            else:
+                # Handle error case
+                dispatcher.utter_message(text="Sorry, there was an issue processing your request.")
+
+        except requests.exceptions.RequestException as e:
+            dispatcher.utter_message(text=f"Error connecting to RAG service: {str(e)}")
+
+        return 
